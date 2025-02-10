18a282f5da430d8395420dbb5f3c95f1d8d9087f
Hemanth Raja <hemanthraja2004@gmail.com>
2025-02-10 16:35:17 +0530

    Navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex flex-col lg:flex-row justify-between items-center py-3 mx-6 mb-10 ">
+      <div>
+        <h3 className="text-xl font-bold text-gray-600">
+          {new Date().toUTCString().slice(0, 16)}
+        </h3>
+        <h1 className="text-2xl font-bold">DC FC Service</h1>
+      </div>
+      <div className="">
+        <input
+          type="search"
+          name="search"
+          id=""
+          placeholder="Search here"
+          autoComplete="off"
+          className="p-3 border border-gray-400 rounded-lg text-sm outline-none w-full lg:w-[25vw]"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
