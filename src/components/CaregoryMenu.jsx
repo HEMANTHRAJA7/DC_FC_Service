820fc907de59aa68f7ca8d2e59738ccb89af1641
Hemanth Raja <hemanthraja2004@gmail.com>
2025-02-18 16:19:00 +0530

    Caregory split

--- src/components/CaregoryMenu.jsx
+++ src/components/CaregoryMenu.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import FoodDate from "../data/FoodData";
 
 const CaregoryMenue = () => {
+  const [categories, setCategories] = useState([]);
+  const listUniqueCategoties = () => {
+    const uniqueCategories = [
+      ...new Set(FoodDate.map((food) => food.category)),
+    ];
+
+    setCategories(uniqueCategories);
+    console.log(uniqueCategories);
+  };
+
+  useEffect(() => {
+    listUniqueCategoties();
+  }, []);
   return (
     <div className="ml-6 mb-6 flex flex-col justify-center items-center">
       <h3 className="text-xl font-semibold">Find the best food</h3>
       <div className="my-5 flex gap-3 flex-wrap">
-        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">All</button>
-        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">Lunch</button>
-        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">Breakfast</button>
-        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">Dinner</button>
-        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">Snacks</button>
+        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">
+          All
+        </button>
+
+        {categories.map((category, id) => {
+          return (
+            <button
+              key={id}
+              className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white"
+            >
+              {category}
+            </button>
+          );
+        })}
+        {/* 
+        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">
+          Lunch
+        </button>
+        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">
+          Breakfast
+        </button>
+        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">
+          Dinner
+        </button>
+        <button className="px-[1rem] py-[0.5rem] bg-[#F5EFFF] font-bold rounded-lg hover:bg-[#A294F9] hover:text-white">
+          Snacks
+        </button> */}
       </div>
     </div>
   );
