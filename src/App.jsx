344f8e84a98256995739d99c77b6088fe3fce97c
Hemanth Raja <hemanthraja2004@gmail.com>
2025-02-19 14:39:10 +0530

    Protected route is setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Route , Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Error from './pages/Error'
 import Success from './pages/Success'
+import ProtectedRoutes from './components/ProtectedRoutes'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/success" element={<Success />} />
+        <Route path="/success" element={<ProtectedRoutes element={<Success />}/>} />
         <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
