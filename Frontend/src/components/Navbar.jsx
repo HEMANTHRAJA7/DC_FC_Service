d8f0e72a7385c0a8ee3063e7c4d76d1d64093067
Hemanth Raja <hemanthraja2004@gmail.com>
2025-04-07 20:55:52 +0530

    History

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -3,16 +3,18 @@ import { useDispatch } from "react-redux";
 import { setSearch } from "../redux/slices/SearchSlice";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
+
 const Navbar = () => {
-  const { user } = useAuthContext()
+  const { user } = useAuthContext();
   const dispatch = useDispatch();
-  const { logout } = useLogout()
-  
-  const handleClick = () =>{
-    logout()
-  }
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
-    <nav className="flex flex-col lg:flex-row justify-between items-center py-3 mx-6 mb-7 ">
+    <nav className="relative flex flex-col lg:flex-row justify-between items-center py-3 mx-6 mb-7">
       <div className="mb-5">
         <h3>{user.username}</h3>
         <h3 className="text-xl font-bold text-gray-600">
@@ -20,17 +22,29 @@ const Navbar = () => {
         </h3>
         <h1 className="text-2xl font-bold">DC FC Service</h1>
       </div>
-      <div className="m-6">
+
+      <button
+        onClick={handleClick}
+        className="absolute top-3 right-0 p-3  bg-[#A294F9] text-white hover:bg-[#500073] rounded-lg cursor-pointer text-lg lg:hidden"
+      >
+        Logout
+      </button>
+
+      <div className="m-6 flex flex-col lg:flex-row lg:items-center">
         <input
           type="search"
           name="search"
-          id=""
           placeholder="Search here"
           autoComplete="off"
-          onChange={(e)=> dispatch(setSearch(e.target.value))}
-          className="p-3 border border-gray-400 bg-[#F5EFFF] rounded-lg text-sm outline-none w-[90vw] lg:w-[25vw] "
+          onChange={(e) => dispatch(setSearch(e.target.value))}
+          className="p-3 border border-gray-400 bg-[#F5EFFF] rounded-lg text-sm outline-none w-[90vw] lg:w-[25vw]"
         />
-        <button onClick={handleClick} className="mx-5 p-2 bg-[#A294F9] text-white hover:bg-[#500073] rounded-lg cursor-pointer text-lg">Logout</button>
+        <button
+          onClick={handleClick}
+          className="hidden lg:block mx-5 p-2 bg-[#A294F9] text-white hover:bg-[#500073] rounded-lg cursor-pointer text-lg"
+        >
+          Logout
+        </button>
       </div>
     </nav>
   );
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,10 +3,12 @@ import Navbar from "../components/Navbar"
 import CategoryMenu from "../components/CaregoryMenu"
 import FoodItems from "../components/FoodItems"
 import Cart from "../components/Cart";
+import History from "../components/History"
 
 const Home = () => {
 
   const [activeCart, setActiveCart] = useState(false);
+  const [activeHistory, setActiveHistory] = useState(false);
 
   return (
     <div className={`${activeCart ? "lg:w-[80vw]" : "lg:w-[100vw]"} transition-all duration-500 ease-in-out`}>
@@ -14,6 +16,7 @@ const Home = () => {
         <CategoryMenu />
         <FoodItems />
         <Cart activeCart={activeCart} setActiveCart={setActiveCart} />
+        <History activeHistory={activeHistory} setActiveCart={setActiveHistory} />
     </div>
   );
 };
